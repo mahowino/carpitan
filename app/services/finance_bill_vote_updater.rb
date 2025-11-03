0379c3cee0f826ec838c3d769cebcdd47963ef21
mahalonowino <mahowino@gmail.com>
2025-11-03 22:46:10 +0300

    Updated approach to fetching finance bill votes to update the votes table instead of a standalone value

--- app/services/finance_bill_vote_updater.rb
+++ app/services/finance_bill_vote_updater.rb
@@ -1,13 +1,13 @@
-require "net/http"
-require "json"
-require "logger"
+require 'net/http'
+require 'json'
+require 'logger'
 
 class FinanceBillVoteUpdater
-  def initialize(api_url: Rails.application.config.api_configs["politician_api"],
+  def initialize(api_url: Rails.application.config.api_configs['politician_api'],
                  api_client: ApiClient, user_finder: -> { User.first })
     @api_url = api_url
     @api_client = api_client
-    @user_finder = user_finder
+    @user = user_finder.call
   end
 
   def call
@@ -19,17 +19,38 @@ class FinanceBillVoteUpdater
   private
 
   def update_or_create_politician(data)
-    politician = Politician.find_or_initialize_by(name: data["NAME"])
-    politician.update!(position: "Member of parliament", jurisdiction: data["relation_name"],
-                       finance_bill_vote: transform_vote(data["vote"]), user: @user_finder.call)
-    logger.info("✅ Saved #{politician.name} with vote: #{politician.finance_bill_vote}")
+    politician = Politician.find_or_initialize_by(name: data['NAME'])
+    politician.update!(position: 'Member of parliament', jurisdiction: data['relation_name'], user: @user)
+    update_finance_bill_vote(data['vote'], politician)
   rescue StandardError => e
-    logger.error("❌ Failed to save #{data['NAME']}: #{e.message}")
+    logger.error("❌ An unexpected error occured for #{data['NAME']}: #{e.message}")
+  end
+
+  def update_finance_bill_vote(data, politician) 
+    result = save_finance_bill_vote(data, politician)
+    if result.is_a?(Vote)
+      logger.info("✅ Saved #{politician.name} with vote: #{politician.finance_bill_vote}")
+    else
+      logger.error("❌ Failed to save #{data['NAME']}: #{result}")
+    end
+  end
+
+  def save_finance_bill_vote(vote, politician)
+    result = Vote.new(
+      choice: transform_vote(vote),
+      politician: politician,
+      legislation: Legislation.finance_bill2024,
+      user_id: @user.id
+    )
+    return "✅ Error saving vote: #{result.errors.full_messages}" unless result.valid?
+
+    result.save!
+    result
   end
 
   def transform_vote(vote)
-    { "Y" => "Yes", "N" => "No", "A" => "Abstain", "U" => "Unavailable" }[vote&.upcase] || vote
+    { 'Y' => 'yes', 'N' => 'no', 'A' => 'abstain', 'U' => 'unavailable' }[vote&.upcase] || vote
   end
 
-  def logger = @logger ||= Logger.new(Rails.root.join("log", "finance_bill_updater.log"))
+  def logger = @logger ||= Logger.new(Rails.root.join('log', 'finance_bill_updater.log'))
 end
--- deployment_checklist.md
+++ deployment_checklist.md
@@ -1,4 +1,4 @@
 <B>How to get politicians</b>
 - rails db:migrate
 - rails db:seed to create default user
-- bin/rails politicians:fetch_and_update
+- bin/rails politicians:fetch_finance_bill_2024_vote
